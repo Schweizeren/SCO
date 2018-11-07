3df250b606a199cf2b8f145ea54389bf9a527f2f
Schweizeren <j_wannerhenriksen@hotmail.com>
2018-11-07 09:08:48 +0100

    Hacking Software updated

---
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,15 @@
+
+package javaapplication6;
+
+
+public class JavaApplication6
+{
+    public String Nijas;
+    
+    public static void main(String[] args)
+    {
+        NewClass.countUp();
+        NewClass.StealInternet();
+    }
+    
+}
---
+++ JavaApplication6/src/javaapplication6/NewClass.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author J
+ */
+public class NewClass
+{
+    public static void countUp()
+    {
+        System.out.println("1%");
+        System.out.println("2%");
+        System.out.println("3%");
+        System.out.println("4%");
+        System.out.println("5%");
+        System.out.println("6%");
+        System.out.println("7%");
+        System.out.println("8%");
+        System.out.println("9%");
+        System.out.println("10%");
+        System.out.println("20%");
+        System.out.println("30%");
+        System.out.println("40%");
+        System.out.println("50%");
+        System.out.println("60%");
+        System.out.println("70%");
+        System.out.println("80%");
+        System.out.println("90%");
+        System.out.println("100%");
+        
+    }
+    
+    public static void StealInternet()
+    {
+        System.out.println("The internet is now yours");
+    }
+}
